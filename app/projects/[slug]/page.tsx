d16eac0ab17b43048b2d83e2a650db0ff8bd7bd9
lukestahl <stahl.luke@web.de>
2022-11-24 12:57:16 +0100

    add projects and about page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,17 +1,46 @@
-import Link from "next/link";
+import Image from "next/image";
 import { Container } from "../../../src/components/container";
+import { GoBackButton } from "../../../src/components/goback.button";
 import { ProjectService } from "../../../src/lib/project.service";
 
-export default async function Projects({ params: { slug } }: { params: { slug: string } }) {
-	const project = await ProjectService.getProjectByFileName(`${slug}.md`);
+export default async function Projects({ params }: { params: { slug: string } }) {
+	const project = await ProjectService.getProjectByFileName(`${params.slug}.md`);
 	return (
-		<div className="mt-28">
+		<div key={project.slug} className="mt-16 md:mt-28">
 			<Container className="dark:text-zinc-200">
-				<h1>Project</h1>
-				<div key={project.slug}>
-					<div>{project.meta.title}</div>
-					<div>{project.meta.description}</div>
-					<div>{project.meta.author}</div>
+				<div className="relative">
+					<div className="left-0 top-0 xl:absolute">
+						<GoBackButton />
+					</div>
+					<article className="mx-auto mt-6 max-w-2xl xl:mt-0">
+						<header className="space-y-6">
+							<p className="relative pl-3 text-zinc-500 dark:text-zinc-400">
+								<span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+									<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+								</span>
+								{project.meta.tags.join(", ")}
+							</p>
+							<h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+								{project.meta.title}
+							</h1>
+							<div className="relative aspect-video">
+								<Image
+									src={project.meta.bannerImage}
+									alt="alt"
+									fill
+									className="rounded-2xl object-cover"
+									{...(project.meta.bannerImageBlur
+										? {
+												placeholder: "blur",
+												blurDataURL: project.meta.bannerImageBlur,
+										  }
+										: {})}
+								/>
+							</div>
+							<p className="text-base text-zinc-600 dark:text-zinc-400">{project.meta.description}</p>
+						</header>
+						<div className="prose mt-8 dark:prose-invert" dangerouslySetInnerHTML={{ __html: project.content }} />
+					</article>
 				</div>
 			</Container>
 		</div>
--- projects/project2.md
+++ projects/project2.md
@@ -2,8 +2,11 @@
 author: 'Luke Stahl'
 date: '2022-09-04'
 title: 'Project 2'
-description: 'This is sample content. The section above is called Frontmatter where we can add post metadata like title and author. You can add as little or as many properties in the frontmatter using YAML syntax.'
-bannerImage: "url-to-image.png"
+description: 'This is sample content. The section above is called Frontmatter where we can add post metadata'
+bannerImage: "/assets/images/projects/testproject.jpg"
+icon: "/assets/logos/nextjs-icon.svg"
+links:
+    - https://google.com/
 tags:
     - example
 ---

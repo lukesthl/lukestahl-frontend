fe3b0f9c44104a13513c4e6b22f5fa0c0e6e8034
lukestahl <stahl.luke@web.de>
2023-05-01 13:27:29 +0200

    dynamic tooltip import

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,4 +1,7 @@
 "use client";
+import dynamic from "next/dynamic";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
-export const Tooltip = (props: React.ComponentProps<typeof ReactTooltip>) => <ReactTooltip {...props} />;
+const TooltipDynamic = dynamic(() => import("react-tooltip").then(mod => mod.Tooltip), { ssr: false });
+
+export const Tooltip = (props: React.ComponentProps<typeof ReactTooltip>) => <TooltipDynamic {...props} />;

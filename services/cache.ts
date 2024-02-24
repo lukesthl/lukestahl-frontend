ca2cd406c395c91cb818966b3796fbc80db51404
lukestahl <stahl.luke@web.de>
2024-02-24 14:49:31 +0100

    remove performance logs

--- services/cache.ts
+++ services/cache.ts
@@ -12,12 +12,10 @@ export class Cache<T> {
 	public async get(key: string): Promise<T | null> {
 		let content: T | null = null;
 		try {
-			const time = Date.now();
 			const fileKey = this.generateHash(key);
 			const filePath = path.join(this.options.path, `${fileKey}.json`);
 			const data = await fs.readFile(filePath, { encoding: "utf8" });
 			content = JSON.parse(data) as T;
-			console.log("time to read cache", Date.now() - time + "ms");
 		} catch (error) {
 			if (error instanceof Error && error.name === "ENOENT") {
 				return null;
@@ -27,11 +25,9 @@ export class Cache<T> {
 	}
 
 	public async set(key: string, value: T): Promise<void> {
-		const time = Date.now();
 		const fileKey = this.generateHash(key);
 		const filePath = path.join(this.options.path, `${fileKey}.json`);
 		await fs.mkdir(this.options.path, { recursive: true });
 		await fs.writeFile(filePath, JSON.stringify(value));
-		console.log("time to write cache", Date.now() - time + "ms");
 	}
 }
